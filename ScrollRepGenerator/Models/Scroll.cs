0f2db33376e0c43af535825bb7ae800aaf412f14
Joseph Convery <Jconvery@nexient.com>
2018-10-15 08:05:35 -0400

    Updated for 2.0

--- ScrollRepGenerator/Models/Scroll.cs
+++ ScrollRepGenerator/Models/Scroll.cs
@@ -13,20 +13,36 @@ namespace ScrollRepGenerator.Models
         public string Name { get; set; }
         public int Level { get; set; }
 
+        public Scroll() { }
+
+        public Scroll(int level, SpellFlavor flavor)
+        {
+            Name = flavor.ToString() + " Bolt";
+            Level = level;
+            Incant = $"{CalculateDamage(level)} {flavor.ToString()} Bolt";
+            Type = SpellType.Evocation;
+        }
+
         public string FetchIncant()
         {
-            switch(Type)
+            switch (Type)
             {
-                case SpellType.Binding: return "With mystic force I " + Incant;
-                case SpellType.Command: return "I command you to " + Incant;
+                case SpellType.Binding: return "With binding force I " + Incant;
+                case SpellType.Commands: return "I command you to " + Incant;
                 case SpellType.EForce: return "With eldritch force I " + Incant;
-                case SpellType.Evocation: return "I call forth " + Incant;
-                case SpellType.Protective: return "I grant you the power " + Incant;
-                case SpellType.SForce: return "I summon a force to " + Incant;
+                case SpellType.Evocation: return "I evoke a " + Incant;
+                case SpellType.Protection: return "I protect you with " + Incant;
+                case SpellType.Fixing: return Incant;
                 default: return "Not a spell type I've heard of" + Incant;
             }
         }
+
+        private string CalculateDamage(int level)
+        { return (level * 5).ToString(); }
+
     }
 
-    public enum SpellType { Binding, Command, EForce, Evocation, Protective, SForce }
+    public enum SpellType { Binding, Commands, EForce, Evocation, Protection, Fixing}
+
+    public enum SpellFlavor { Stone, Lighting, Ice, Flame }
 }
